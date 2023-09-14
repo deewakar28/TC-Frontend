02b52b9f29df38bebb4d26741fc3d2c845e578f4
Deewakar <deewakarrao59@gmail.com>
2023-09-14 11:47:05 +0530

    problem statement changes

---
+++ src/assets/Techteam_data.js
@@ -0,0 +1,98 @@
+export const techteamList = [
+    {
+      id: 1,
+      name: "Rishav Gandhi ",
+      imgUrl: Rishav_Gandhi,
+      linkedinUrl: "https://www.linkedin.com/in/rishav-gandhi-998a74211/",
+      instaUrl: "https://www.instagram.com/___rishav._/",
+      desi: "Head Coordinator"
+    },
+    {
+        id: 2,
+        name: "Aniket Kumar",
+        imgUrl: Aniket_Kumar,
+        linkedinUrl: "https://www.linkedin.com/in/kraniket901/",
+        instaUrl: "#",
+        desi: "Core Coordinator"
+    },
+    {
+        id: 3,
+        name: "Vibhuti Chandrakar",
+        imgUrl: Vibhuti_Chandrakar,
+        linkedinUrl: "https://www.linkedin.com/in/vibhuti-chandrakar-4b1bb024b",
+        instaUrl: "https://www.instagram.com/vibhuti_.01/",
+        desi: "Core Coordinator"
+    },
+    {
+        id: 4,
+        name: "Piyush Kumar",
+        imgUrl: Piyush_Kumar,
+        linkedinUrl: "https://www.linkedin.com/in/piyush-in/",
+        instaUrl: "",
+        desi: "Core Coordinator"
+    },
+  
+    {
+        id: 5,
+       name: "Ankit Kumar Lakra",
+       imgUrl: Ankit_Kumar_Lakra,
+       linkedinUrl: "https://www.linkedin.com/in/ankitklakra/",
+       instaUrl: "https://www.instagram.com/ankitklakra/",
+       desi: "Core Coordinator"
+    },
+    {
+        
+    },
+    {
+      id: 6,
+      name: "B. Deewakar Rao",
+      imgUrl: B_Deewakar_Rao,
+      linkedinUrl: "https://www.linkedin.com/in/b-deewakar-rao-960438280/",
+      instaUrl: "https://www.instagram.com/dxxwakr/",
+      desi:"Executive"
+    },
+    {
+      
+    },
+    {
+      id: 7,
+      name: "Nehil Sahu",
+      imgUrl: Nehil_Sahu,
+      linkedinUrl: "https://www.linkedin.com/in/nehil-sahu-35811a1b7/",
+      instaUrl: "https://instagram.com/nehil_sahu?igshid=MzMyNGUyNmU2YQ==",
+      desi: "Executive"
+    },
+    {
+      id: 8,
+      name: "Prabhat Sharma",
+      imgUrl: Prabhat_Sharma,
+      linkedinUrl: "https://www.linkedin.com/in/prabhatsharma2508/",
+      instaUrl: "https://www.instagram.com/prabhat_sharma2508/",
+      desi: "Executive"
+    },
+    {
+      id: 9,
+      name: "Mayank Sahu",
+      imgUrl: Mayank_Sahu,
+      linkedinUrl: "",
+      instaUrl: "https://www.instagram.com/mayank_sahu1005/",
+      desi: "Executive"
+    },
+    {
+        id: 10,
+        name: "Karan Singh",
+        imgUrl: Karan_Singh,
+        linkedinUrl: "https://www.linkedin.com/in/karan-singh-037194260/",
+        instaUrl: "",
+        desi: "Executive"
+    },
+    {
+        id: 11,
+        name: "Anupam Malviya ",
+        imgUrl: Anupam_Malviya ,
+        linkedinUrl: "https://www.linkedin.com/in/anupammalviya/",
+        instaUrl: "https://www.instagram.com/anupam.malviya.167/",
+        desi: "Executive"
+    }
+  
+  ];
